33339a995b5df6c0e169cf2de591fe66c349c37f
C-Mauro <maurocamila744@gmail.com>
2022-07-11 14:20:41 -0700

    null

--- _printf.c
+++ _printf.c
@@ -32,7 +32,7 @@ int _printf(const char *format, ...)
 	int i = 0, count = 0;
 	va_list args;
 
-	if (format == NULL && format[i] != '\0')
+	if (format == NULL || format[i] != '\0')
 		return(-1);
 	
 	va_start(args, format);
